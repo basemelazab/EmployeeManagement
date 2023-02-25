76a72005bbaa37a0a0b79706d85cefbc9b08d26e
basemelazab <basem.elazab2014@gmail.com>
2023-02-25 02:19:53 +0200

    Add project files.

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeeManagement.Models;
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository=employeeRepository;
+        }
+        public string Index()
+        {
+            return _employeeRepository.GetEmployee(3).Name;
+        }
+        public ObjectResult Details()
+        {
+            Employee model = _employeeRepository.GetEmployee(1);
+            return new ObjectResult(model);
+        }
+    }
+}
---
+++ EmployeeManagement/Models/MokeEmployeeRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EmployeeManagement.Models
+{
+    public class MokeEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+        public MokeEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee(){Id = 1, Name ="Ahmed",Department="IT",Email="Ahmed@gmail.com"},
+                new Employee(){Id = 2, Name ="Mohamed",Department ="HR",Email="Mohamed@gmail.com"},
+                 new Employee(){Id = 3, Name ="Basem",Department ="IT",Email="Basem@gmail.com"},
+            };
+        }
+        public Employee GetEmployee(int id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
